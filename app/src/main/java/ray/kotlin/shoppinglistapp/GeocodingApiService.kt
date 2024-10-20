087c1e2551d76e27b3cf3459b814c0408779b284
Chhrone <rayinailham9@gmail.com>
2024-10-20 12:01:15 +0700

    Hi

---
+++ app/src/main/java/ray/kotlin/shoppinglistapp/GeocodingApiService.kt
@@ -0,0 +1,13 @@
+package ray.kotlin.shoppinglistapp
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GeocodingApiService {
+
+    @GET("maps/api/geocode/json")
+    suspend fun getAddressFromCoordinates(
+        @Query("latlng") latlng : String,
+        @Query("key") apiKey : String
+    ): GeocodingResponse
+}
---
+++ app/src/main/java/ray/kotlin/shoppinglistapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package ray.kotlin.shoppinglistapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import ray.kotlin.shoppinglistapp.ui.theme.ShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppingListAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation()
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    val viewModel: LocationViewModel = viewModel()
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+
+    NavHost(navController = navController, startDestination = "shoppinglistscreen") {
+        composable("shoppinglistscreen") {
+            ShoppingListApp(
+                locationUtils = locationUtils,
+                viewModel = viewModel,
+                navController = navController,
+                context = context,
+                address = viewModel.address.value.firstOrNull()?.formatted_address?: "no address"
+            )
+        }
+
+        dialog("locationscreen"){ backstack ->
+            viewModel.location.value?.let { it1 ->
+                LocationSelectionScreen(location = it1, onLocationSelected = {
+                    viewModel.fetchAddress("${it.latitude}, ${it.longitude}")
+                    navController.popBackStack()
+                })
+            }
+        }
+    }
+}
